90aa906c2dbcb17b8639d8a7180667633a266b75
teamlance23 <himanshu.sharma@teamlance.io>
2021-09-17 12:42:08 +0530

    first commit

---
+++ lib/models/authorization_options.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'authorization_options.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthorizationOptions _$AuthorizationOptionsFromJson(
+        Map<String, dynamic> json) =>
+    AuthorizationOptions();
+
+Map<String, dynamic> _$AuthorizationOptionsToJson(
+        AuthorizationOptions instance) =>
+    <String, dynamic>{};
--- lib/pusher_beamer.dart
+++ lib/pusher_beamer.dart
@@ -10,4 +10,42 @@ class PusherBeamer {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+  static Future<void> start(String instanceId) async {
+    await _channel.invokeMethod('start', instanceId);
+  }
+
+  static Future<void> addDeviceInterest(String interest) async {
+    await _channel.invokeMethod('addDeviceInterest', interest);
+  }
+
+  static Future<void> removeDeviceInterest(String interest) async {
+    await _channel.invokeMethod('removeDeviceInterest', interest);
+  }
+
+  static Future<List<String>> getDeviceInterests() async {
+    final List<String>? interests =
+        await _channel.invokeListMethod('getDeviceInterests');
+
+    return Future.value(interests);
+  }
+
+  static Future<void> setDeviceInterests(List<String> interests) async {
+    await _channel.invokeMethod('setDeviceInterests', interests);
+  }
+
+  static Future<void> clearDeviceInterests() async {
+    await _channel.invokeMethod('clearDeviceInterests');
+  }
+
+  static Future<bool> setUserId(String userId, tokenProvider) {
+    throw UnimplementedError('This method is still unimplemented');
+  }
+
+  static Future<void> clearAllState() async {
+    await _channel.invokeMethod('clearAllState');
+  }
+
+  static Future<void> stop() async {
+    await _channel.invokeMethod('stop');
+  }
 }
